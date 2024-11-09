96d532fdd0211d33e0962174e95153f95e9e9c97
sriseptani29 <164107995+sriseptani29@users.noreply.github.com>
2024-11-10 05:15:02 +0700

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// D:\react_sriseptiani\config.js
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('contact_form', 'username', 'password', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-module.exports = sequelize;
